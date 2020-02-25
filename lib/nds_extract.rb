0a8824f9f737d14a70de52ea1d7a3d2f368e41a5
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-25 17:04:48 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,14 +22,14 @@ end
 
 
 def directors_totals(nds)
-  result = {}
+  totals = {}
   director_index = 0
   while director_index < nds.length do
     director = nds[director_index]
-    result[director[:name]] = gross_for_director(director)
+    totals[director[:name]] = gross_for_director(director)
     director_index += 1
   end
-  result
+  totals
 end
 
 
